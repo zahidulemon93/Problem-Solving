327d03b70a1ea8613d3f6d692816a83ef1373bcd
zahidulemon93 <35254810+zahidulemon93@users.noreply.github.com>
2022-08-31 01:56:51 +0600

    Create Solution2.cs

---
+++ LeetCode/Easy/1. Two Sum/C#/Solution2.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int,int> dic = new Dictionary<int,int>();
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(dic.ContainsKey(target - nums[i]))
+                return new int[]{i, dic[target - nums[i]]};
+            
+            else if(!dic.ContainsKey(nums[i]))
+                dic.Add(nums[i], i);
+        }
+        
+        return new int[2];
+    }
+}
